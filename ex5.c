2aa0fe6f1cd9786dea497ea716e6d285dbc3d5a0
taich <r202203475ln@jindai.jp>
2024-01-14 19:36:15 +0900

    main

--- ex5.c
+++ ex5.c
@@ -5,20 +5,3 @@ int main() {
     return 0;
 }
 
-#include <stdio.h>
-
-int main() {
-    char str[] = "hello";
-    printf("%s\n", str);
-    return 0;
-}
-
-#include <stdio.h>
-#include <string.h>
-
-int main() {
-    char str[] = "hello";
-    printf("String: %s\n", str);
-    printf("Length: %lu\n", strlen(str));
-    return 0;
-}
